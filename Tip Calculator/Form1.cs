7bced5e9305227d86cc2d431c521181264470431
anshikajain108 <61621029+anshikajain108@users.noreply.github.com>
2021-06-21 20:58:29 +0530

    Add files via upload

---
+++ Tip Calculator/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tip_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double n1 = Convert.ToDouble(numericUpDown1.Text);
+            double n2 = Convert.ToDouble(numericUpDown2.Text);
+
+            double Tip = (n1 / 100) * txt1 / n2; //calculating the tip per person
+            double Total = (txt1 / n2) + Tip;    //calculating total amount to be paid per person
+            textBox2.Text = Tip.ToString();
+            textBox3.Text = Total.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }   
+ }
